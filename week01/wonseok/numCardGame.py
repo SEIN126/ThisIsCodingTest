cb6abdf79a4b96b28fe1d8d3cf14bb1d04c0ac2b
WonSeok Jeong <g0401828t@postech.ac.kr>
2021-08-19 17:10:55 +0900

    Update numCardGame.py

--- week01/wonseok/numCardGame.py
+++ week01/wonseok/numCardGame.py
@@ -1,1 +1,13 @@
+N, M = map(int, input().split())
 
+result = 0
+for n in range(N):
+  list_ = list(map(int, input().split()))
+  
+  # 현재 줄의 min 값
+  min_ = min(list_)
+  
+  # 전고 현재 줄의 min 값 중에서 max 값
+  result = max(min_, result)
+
+print(result)
