9716a0be83c6be29406d4977a975cd60df836936
WonSeok Jeong <g0401828t@postech.ac.kr>
2021-09-06 20:30:24 +0900

    *****Solution

--- week_02/wonseok/외벽점검.py
+++ week_02/wonseok/외벽점검.py
@@ -1,35 +1,36 @@
 from itertools import permutations
 
 def solution(n, weak, dist):
-  answer = len(dist) + 1  ## 친구들을 다 투입 해도 안 될 경우 생각해서 초기화
+  # 친구들 다 투입 해도 안될때를 위해 초기화
+  answer = len(dist) + 1
 
-  num_weak = len(weak)  # weak의 전체 갯수
-  # 원형을 일자 형태로 바꾼다.
-  for l in range(num_weak):
-    weak.append(weak[l] + n)
+  # 원형의 weak를 일자로 펴준다
+  len_weak = len(weak)
+  for w in range(len_weak):
+    weak.append(weak[w] + n)
 
-  # friends 순열조합
-  friends_list = list(permutations(dist, len(dist)))
+  for start in range(len_weak):
+    # 모든 친구의 순열
+    for friends in permutations(dist, len(dist)):
+      count = 1  # 시작 지점마다 필요한 친구의 수 초기화
+      next_pos = weak[start] + friends[count - 1]  #첫번째 친구가 이동 할 수 있는 위치
 
-  for start in range(num_weak):
-    for friends in friends_list:
-      count = 1  # 투입한 친구의 수
-      position = weak[start] + friends[count-1]  # 첫번째 투입한 친구가 갈 수 있는 위치
-
-      for index in range(start, start+num_weak):
-        if position < weak[index]:
-          count += 1  # 다음 친구 투입
-          if count < len(dist):  # 다음 친구 투입이 불가할 경우
+      # 갈 수 있는 위치와 모든 weak 확인
+      for index in range(start, start + len_weak):
+        # 다음 weak까지 갈 수 없다면 다음 친구 투입
+        if next_pos < weak[index]:
+          count += 1
+          # 더 이상 투입할 친구가 없다면
+          if count > len(dist):
             break
-          position = weak[index] + friends[count-1]
-
+          next_pos = weak[index] + friends[count - 1]  # 다음 친구가 갈 수 있는 위치
       answer = min(answer, count)
-      
 
   if answer > len(dist):
     return -1
+  else:
+    return answer
 
-  return answer
 
 print(solution(12, [1,5,6,10], [1,2,3,4]))
 print(solution(12, [1,3,4,9,10], [3,5,7]))
